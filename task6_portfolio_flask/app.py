849db906b99e78531e2318a915497661726a1ce1
divya-123-TC <d76469224@gmail.com>
2025-11-24 18:51:32 +0530

    Add files via upload

---
+++ task6_portfolio_flask/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        message = request.form["message"]
+
+        print("\n===== NEW MESSAGE RECEIVED =====")
+        print("Name:", name)
+        print("Email:", email)
+        print("Message:", message)
+        print("================================\n")
+
+        return render_template("contact.html", success=True)
+
+    return render_template("contact.html", success=False)
+
+if __name__ == "__main__":
+    app.run(debug=True)
